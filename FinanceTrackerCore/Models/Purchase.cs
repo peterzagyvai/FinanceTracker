717dce8a4d16a23e54f9eae3af66c767b4d49228
Peter Zagyvai <p.zagyvai@gmail.com>
2025-06-24 12:29:39 +0200

    Added doc-comments to Purchase.cs

--- FinanceTrackerCore/Models/Purchase.cs
+++ FinanceTrackerCore/Models/Purchase.cs
@@ -39,6 +39,12 @@ public class Purchase : TransactionSource
         _purchasedItems = new();
     }
 
+    /// <summary>
+    /// Adds a new purchasablew item to the PurchasedItems collection
+    /// </summary>
+    /// <param name="item">The item that will be added to the collection</param>
+    /// <param name="amount">The amount of the items added to the colelction</param>
+    /// <param name="pricePerUnit">The current price of the item per unit</param>
     public void AddItem(Item item, decimal amount, Money pricePerUnit)
     {
         _purchasedItems.Add(
@@ -49,11 +55,20 @@ public class Purchase : TransactionSource
         To.TransactionAmount = TotalPriceInCurrency(To.TransactionAmount.CurrencyISO);
     }
 
+    /// <summary>
+    /// Removes an item from the Purchased items collection
+    /// </summary>
+    /// <param name="item">The item that will be removed from the colelction</param>
     public void RemoveItem(PurchasedItem item)
     {
         _purchasedItems.Remove(item);
     }
 
+    /// <summary>
+    /// Calculates the sum price of the purchsed items in a pre defiend currency
+    /// </summary>
+    /// <param name="isoCode">The iso code of the currency</param>
+    /// <returns>The sum price of the pruchaed items</returns>
     public Money TotalPriceInCurrency(string isoCode)
     {
         decimal sumPrice = 0;
